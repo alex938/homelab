d37055347d8bb73a57bf033a8fdf54b393aae925
alex <alex@slash-root.com>
2024-11-04 18:57:07 +0000

    added dev2 box

--- terraform/commands.md
+++ terraform/commands.md
@@ -13,8 +13,8 @@ terraform init
 terraform plan
 
 Destroy:
-terraform apply -var="create_vm=false" -auto-approve
+terraform apply -var="create_dev2_vm=false" -auto-approve -target=proxmox_vm_qemu.dev2_labjunkie_org
 
 Create:
-terraform apply -var="create_vm=true" -auto-approve
+terraform apply -var="create_dev2_vm=true" -auto-approve -target=proxmox_vm_qemu.dev2_labjunkie_org
 ```
--- terraform/dev2.labjunkie.org.tf
+++ terraform/dev2.labjunkie.org.tf
@@ -1,21 +1,21 @@
-variable "create_kubecontrol_vm" {
+variable "create_dev2_vm" {
   type    = bool
   default = true
 }
 
-resource "proxmox_vm_qemu" "kubecontrol1" {
-    count       = var.create_kubecontrol_vm ? 1 : 0
-    name        = "kubecontrol1"
-    desc        = "kubernetes control server 1"
-    vmid        = "201"
+resource "proxmox_vm_qemu" "dev2_labjunkie_org" {
+    count       = var.create_dev2_vm ? 1 : 0
+    name        = "dev2.labjunkie.org"
+    desc        = "dev2.labjunkie.org"
+    vmid        = "100"
     target_node = "ms01"
 
     agent       = 1
     clone       = "ubuntubase"
-    cores       = 2
+    cores       = 4
     sockets     = 2
     cpu         = "host"
-    memory      = 2048
+    memory      = 4096
     full_clone  = true
 
     scsihw = "virtio-scsi-single"
@@ -39,8 +39,7 @@ resource "proxmox_vm_qemu" "kubecontrol1" {
     os_type     = "cloud-init"
     ciuser      = "alex"
     cipassword  = "$6$c/lkMtwWENjZ1QiM$x0tkiAz1PnVcKgajgqTPSvW.dvR.jwodsyQr.XSrG2SwVKJ1JzhAabQoQMz2MfZgDmipAFA46L65ckOVxszHA0" #"alex" changed via ansible scripts
-    ipconfig0   = "ip=192.168.2.60/24,gw=192.168.2.1"
-    nameserver  = "192.168.2.12,172.20.1.2"
+    ipconfig0   = "ip=192.168.2.47/24,gw=192.168.2.1,dns=192.168.2.12 172.20.1.2"
 
     network {
         model  = "virtio"
